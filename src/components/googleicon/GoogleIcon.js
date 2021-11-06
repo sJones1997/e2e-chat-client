8d44fae6818eb92cb844b33ee726d457386aa866
Sean <sean.g.jones97@gmail.com>
2021-11-06 12:42:16 +0000

    finished main features, working on mobile css

--- src/components/googleicon/GoogleIcon.js
+++ src/components/googleicon/GoogleIcon.js
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGoogle } from "@fortawesome/free-brands-svg-icons"
-
+import {baseApi} from '../../app/App';
 
 export default function GoogleIcon(){
     return (
         <div>
-            <span><FontAwesomeIcon icon={faGoogle} /></span>            
+            <span><a href={`${baseApi}/auth/google`}><FontAwesomeIcon icon={faGoogle} /></a></span>            
         </div>    
     )
 }
