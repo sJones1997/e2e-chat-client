b5ce920cd97ff0a498c979587b1fda9c06d6b1d9
Sean <sean.g.jones97@gmail.com>
2021-11-07 10:48:51 +0000

    adding in viewport change function

--- src/app/App.js
+++ src/app/App.js
@@ -9,6 +9,7 @@ import Login from '../features/login/Login';
 import Chatroom from '../features/chatroom/Chatroom';
 import socketIOCient from 'socket.io-client';
 import { useEffect } from 'react';
+import { faWindows } from '@fortawesome/free-brands-svg-icons';
 export const baseApi = `${process.env.REACT_APP_API_URL}/api`;
 export const socket = socketIOCient(`${process.env.REACT_APP_API_URL}`, {
   withCredentials: true
@@ -17,11 +18,21 @@ socket.connect();
 
 function App() {
 
+
   useEffect(() => {
-    let viewHeight = window.innerHeight;
-    let viewWidth = window.innerWidth;
-    let viewport = document.querySelector("meta[name=viewport]");
-    viewport.setAttribute("content", "height=" + viewHeight + "px, width=" + viewWidth + "px, initial-scale=1.0");
+    const changeSize = () => {
+      let viewport = document.querySelector("meta[name=viewport]");  
+      viewport.setAttribute("content", "width=device-width, initial-scale=1");   
+      let viewHeight = window.innerHeight;
+      let viewWidth = window.innerWidth;    
+      viewport.setAttribute("content", "height=" + viewHeight + "px, width=" + viewWidth + "px, initial-scale=1.0");      
+    }  
+  
+    window.addEventListener('load', changeSize);
+    window.addEventListener('resize', changeSize);
+    return () => {
+      window.removeEventListener('resize');      
+    }
   }, [])
 
   return (
