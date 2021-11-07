da1f6ff14b425fe693c798cbb34b165f9262a17e
Sean <sean.g.jones97@gmail.com>
2021-11-07 09:54:06 +0000

    changing so app height doesn't shrink on keyboard open

--- src/app/App.js
+++ src/app/App.js
@@ -8,6 +8,7 @@ import Register from '../features/register/Register';
 import Login from '../features/login/Login';
 import Chatroom from '../features/chatroom/Chatroom';
 import socketIOCient from 'socket.io-client';
+import { useEffect } from 'react';
 export const baseApi = `${process.env.REACT_APP_API_URL}/api`;
 export const socket = socketIOCient(`${process.env.REACT_APP_API_URL}`, {
   withCredentials: true
@@ -15,6 +16,13 @@ export const socket = socketIOCient(`${process.env.REACT_APP_API_URL}`, {
 socket.connect();
 
 function App() {
+
+  useEffect(() => {
+    let viewHeight = window.innerHeight;
+    let viewWidth = window.innerWidth;
+    let viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", "height=" + viewHeight + "px, width=" + viewWidth + "px, initial-scale=1.0");
+  }, [])
 
   return (
     <div className="App">
